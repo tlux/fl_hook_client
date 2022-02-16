f65584268710efcf19d6fd197cb0262f2309c53c
Tobias Casper <tobias.casper@gmail.com>
2022-02-16 21:34:47 +0100

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FLHook.MixProject do
   def project do
     [
       app: :fl_hook_client,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
